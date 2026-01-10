79d46da2d0f23c2d77820238daba2c9c3ab6cb64
TouchOfDarkness <118917318+TouchOfDarkness@users.noreply.github.com>
2026-01-10 17:18:42 +0100

    Screen test

--- Rower_elektryczny/Core/Src/main.c
+++ Rower_elektryczny/Core/Src/main.c
@@ -27,6 +27,7 @@
 #include "quadspi.h"
 #include "gpio.h"
 #include "fmc.h"
+#include "app_touchgfx.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -184,6 +185,7 @@ int main(void)
   MX_I2C4_Init();
   MX_I2C2_Init();
   MX_CRC_Init();
+  MX_TouchGFX_Init();
   /* USER CODE BEGIN 2 */
   // 1. Konfiguracja Filtra - akceptujemy WSZYSTKO (tryb otwarty dla testów)
     // VESC wysyła ramki z ID rozszerzonym (29-bit).
@@ -232,6 +234,7 @@ int main(void)
   {
     /* USER CODE END WHILE */
 
+  MX_TouchGFX_Process();
     /* USER CODE BEGIN 3 */
 	  MPU6050_Read_All(&hi2c2, &MPU6050);
 	  // 1. Pobierz obroty
